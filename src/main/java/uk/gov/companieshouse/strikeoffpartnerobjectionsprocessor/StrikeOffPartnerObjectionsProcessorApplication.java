2b78aa77f6c1b11a465961894fc419e932cfc457
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-05-20 15:29:17 +0100

    TRACS-35 - tabs changed to spaces

--- src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/StrikeOffPartnerObjectionsProcessorApplication.java
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/StrikeOffPartnerObjectionsProcessorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StrikeOffPartnerObjectionsProcessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrikeOffPartnerObjectionsProcessorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StrikeOffPartnerObjectionsProcessorApplication.class, args);
+    }
 
 }
