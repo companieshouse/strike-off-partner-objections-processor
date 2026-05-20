f174d32911b440e6133b4992c0c77bf02e7b94f9
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-05-20 14:37:19 +0100

    TRACS-35 - initial project skeleton

---
+++ src/main/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/StrikeOffPartnerObjectionsProcessorApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.strikeoffpartnerobjectionsprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrikeOffPartnerObjectionsProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrikeOffPartnerObjectionsProcessorApplication.class, args);
+	}
+
+}
