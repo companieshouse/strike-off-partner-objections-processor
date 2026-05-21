1ef989c71ec48a1fd2e960b8e94530241784227f
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-05-21 15:42:03 +0100

    TRACS-30 - healthcheck endpoint and tests added

--- src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/StrikeOffPartnerObjectionsProcessorApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/StrikeOffPartnerObjectionsProcessorApplicationTests.java
@@ -1,13 +1,45 @@
 package uk.gov.companieshouse.strikeoffpartnerobjectionsprocessor;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class StrikeOffPartnerObjectionsProcessorApplicationTests {
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void healthcheckEndpointReturnsUp() throws Exception {
+        mockMvc.perform(get("/strike-off-partner-objections-processor/healthcheck"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+
+    @Test
+    void defaultActuatorHealthPathIsNotExposed() throws Exception {
+        mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isNotFound());
     }
 
 }
