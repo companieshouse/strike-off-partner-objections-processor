a4c2a556f1d21daa839c4a7a6b03ee165a836627
mmodzelewski1-ch <mmodzelewski1@companieshouse.gov.uk>
2026-07-20 10:37:48 +0100

    TRACS-87 - wildcard removed from imports

--- src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/processor/AbstractStrikeOffPartnerEventsProcessorTest.java
+++ src/test/java/uk/gov/companieshouse/strikeoffpartnerobjectionsprocessor/processor/AbstractStrikeOffPartnerEventsProcessorTest.java
@@ -12,7 +12,12 @@ import uk.gov.companieshouse.strikeoff.partner.objections.EventType;
 import uk.gov.companieshouse.strikeoff.partner.objections.StrikeOffPartnerObjections;
 import uk.gov.companieshouse.strikeoffpartnerobjectionsprocessor.exceptions.InvalidStrikeOffMessageException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
